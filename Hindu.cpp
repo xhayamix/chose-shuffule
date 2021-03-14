13a1304a416de2187596eacb42642ac34791a2a4
鈴木稜太朗 <srro1991@gmail.com>
2021-03-15 02:24:35 +0900

    エラー排除

--- Hindu.cpp
+++ Hindu.cpp
@@ -7,6 +7,7 @@ Hindu::Hindu(const InitData& init) : IScene(init){
 	for (int i = 0; i < 10; i++) {
 		buttons.emplace_back(String(1, str[i]), i, Rect(43*(i+1)+i*80, 550, 80, 60));
 	}
+
 	buttons.emplace_back(U"10", 10, Rect(43 * (4 + 1) + 4 * 80 + 160 + 43 + 5, 652, 80, 60));
 	buttons.emplace_back(U"OK", 11, Rect(43 * (4 + 1) + 4 * 80 + 240 + 43 + 10, 652, 80, 60));
 	buttons.emplace_back(U"reset", 12, Rect(43 * (4 + 1) + 4 * 80 + 320 + 43 + 15, 652, 180, 60));
@@ -15,7 +16,6 @@ Hindu::Hindu(const InitData& init) : IScene(init){
 		RectF rect(15 + i % 15 * (pack.width() + 15), 15 + (i / 15) * (pack.height() + 15), 69, 111.64435);
 		rects.push_back(rect);
 	}
-	
 
 }
 
@@ -25,11 +25,15 @@ void Hindu::update(){
 		if (button.rect().leftClicked()) {
 			int num = button.value;
 			if (num == 11) {
-				cards = shuffle(cards,Parse<int>(text));
+				if (text.size() > 0) {
+					cards = shuffle(cards, Parse<int>(text));
+					
+				}
 				break;
 			}
 			if (num == 12) {
 				setCards();
+				text.erase(0, text.size());
 				break;
 			}
 			if (num == 10) {
@@ -45,6 +49,7 @@ void Hindu::update(){
 			}
 		}
 	}
+
 	for (int i = 0; i < rects.size(); i++) {
 		if (rects[i].leftClicked()) {
 			savei = i;
@@ -61,7 +66,6 @@ void Hindu::update(){
 void Hindu::draw() const{
 	for (int i = 0; i < 60; i++) {
 		const Vec2 center(15 + i % 15 * (pack.width() + 15), 15 + (i / 15) * (pack.height() + 15));
-		
 		pack(cards[i]).draw(center);
 	}
 	
@@ -75,6 +79,7 @@ void Hindu::draw() const{
 		}
 		
 	}
+
 	Rect displayRect(43 * (4 + 1) + 4 * 80, 652, 203, 60);
 	displayRect.stretched(-1).draw();
 	FontAsset(U"Number")(text).draw(Arg::rightCenter = displayRect.rightCenter().movedBy(-15, 0), ColorF(0.25));
