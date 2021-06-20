b935b997e5f5988a801557199764e80bebdb0333
鈴木稜太朗 <srro1991@gmail.com>
2021-06-20 20:18:41 +0900

    タイトルに選択肢追加

--- Title.cpp
+++ Title.cpp
@@ -8,8 +8,10 @@ Title::Title(const InitData& init) : IScene(init){
 void Title::update() {
     m_startTransition.update(m_startButton.mouseOver());
     m_exitTransition.update(m_exitButton.mouseOver());
+    m_gameTransition.update(m_gameButton.mouseOver());
 
-    if (m_startButton.mouseOver() || m_exitButton.mouseOver()){
+
+    if (m_startButton.mouseOver() || m_exitButton.mouseOver() || m_gameButton.mouseOver()){
         Cursor::RequestStyle(CursorStyle::Hand);
     }
 
@@ -31,8 +33,10 @@ void Title::draw() const {
 
     m_startButton.draw(ColorF(1.0, m_startTransition.value())).drawFrame(2);
     m_exitButton.draw(ColorF(1.0, m_exitTransition.value())).drawFrame(2);
+    m_gameButton.draw(ColorF(1.0, m_gameTransition.value())).drawFrame(2);
 
     FontAsset(U"Menu")(U"ヒンズーシャッフル").drawAt(m_startButton.center(), ColorF(0.25));
     FontAsset(U"Menu")(U"ファローシャッフル").drawAt(m_exitButton.center(), ColorF(0.25));
+    FontAsset(U"Menu")(U"神経衰弱").drawAt(m_gameButton.center(), ColorF(0.25));
 
 }
--- Title.hpp
+++ Title.hpp
@@ -10,6 +10,9 @@ class Title : public MyApp::Scene {
         Rect m_exitButton = Rect(Arg::center = Scene::Center().movedBy(0, 100), 300, 60);
         Transition m_exitTransition = Transition(0.4s, 0.2s);
 
+        Rect m_gameButton = Rect(Arg::center = Scene::Center().movedBy(0, 200), 300, 60);
+        Transition m_gameTransition = Transition(0.4s, 0.2s);
+
     public:
 
         Title(const InitData& init);
