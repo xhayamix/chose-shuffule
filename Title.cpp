9e98348da6ee336e3c2655fcf8faa6974dd06b2f
BuildTools <unconfigured@null.spigotmc.org>
2022-04-24 13:44:53 +0900

    title画面の編集

--- Title.cpp
+++ Title.cpp
@@ -1,33 +1,25 @@
 #pragma once
 # include "Title.hpp"
 
-Title::Title(const InitData& init) : IScene(init){
+Title::Title(const InitData& init) : IScene(init) {
 
 }
 
 void Title::update() {
-    m_startTransition.update(m_startButton.mouseOver());
-    m_exitTransition.update(m_exitButton.mouseOver());
+
     m_gameTransition.update(m_gameButton.mouseOver());
-    m_plotTransition.update(m_plotButton.mouseOver());
+    m_agentTransition.update(m_agentButton.mouseOver());
 
 
-    if (m_startButton.mouseOver() || m_exitButton.mouseOver() || m_gameButton.mouseOver() || m_plotButton.mouseOver()){
+    if (m_gameButton.mouseOver() || m_agentButton.mouseOver()) {
         Cursor::RequestStyle(CursorStyle::Hand);
     }
 
-    if (m_startButton.leftClicked()){
-        changeScene(State::Hindu, 0.3s);
-    }
-
-    if (m_exitButton.leftClicked()){
-        changeScene(State::Farrow, 0.3s);
-    }
     if (m_gameButton.leftClicked()) {
         changeScene(State::SetGame, 0.3s);
     }
-    if (m_plotButton.leftClicked()) {
-        changeScene(State::Plot, 0.3s);
+    if (m_agentButton.leftClicked()) {
+        changeScene(State::AgentSetGame, 0.3s);
     }
 }
 
@@ -37,14 +29,10 @@ void Title::draw() const {
     FontAsset(U"Title")(titleText).drawAt(center.movedBy(4, 6), ColorF(0.0, 0.5));
     FontAsset(U"Title")(titleText).drawAt(center);
 
-    m_startButton.draw(ColorF(1.0, m_startTransition.value())).drawFrame(2);
-    m_exitButton.draw(ColorF(1.0, m_exitTransition.value())).drawFrame(2);
     m_gameButton.draw(ColorF(1.0, m_gameTransition.value())).drawFrame(2);
-    m_plotButton.draw(ColorF(1.0, m_plotTransition.value())).drawFrame(2);
+    m_agentButton.draw(ColorF(1.0, m_agentTransition.value())).drawFrame(2);
 
-    FontAsset(U"Menu")(U"Hindu Shuffle").drawAt(m_startButton.center(), ColorF(0.25));
-    FontAsset(U"Menu")(U"Farrow Shuffle").drawAt(m_exitButton.center(), ColorF(0.25));
     FontAsset(U"Menu")(U"Memory Game").drawAt(m_gameButton.center(), ColorF(0.25));
-    FontAsset(U"Menu")(U"Plot Shuffle").drawAt(m_plotButton.center(), ColorF(0.25));
+    FontAsset(U"Menu")(U"Agent Memory Game").drawAt(m_agentButton.center(), ColorF(0.25));
 
 }
--- Title.hpp
+++ Title.hpp
@@ -2,24 +2,17 @@
 #include "Common.hpp"
 
 class Title : public MyApp::Scene {
-    private:
+private:
 
-        Rect m_startButton = Rect(Arg::center = Scene::Center().movedBy(0, 0), 300, 60);
-        Transition m_startTransition = Transition(0.4s, 0.2s);
+    Rect m_gameButton = Rect(Arg::center = Scene::Center().movedBy(0, 0), 300, 60);
+    Transition m_gameTransition = Transition(0.4s, 0.2s);
+    Rect m_agentButton = Rect(Arg::center = Scene::Center().movedBy(0, 100), 300, 60);
+    Transition m_agentTransition = Transition(0.4s, 0.2s);
 
-        Rect m_exitButton = Rect(Arg::center = Scene::Center().movedBy(0, 100), 300, 60);
-        Transition m_exitTransition = Transition(0.4s, 0.2s);
+public:
 
-        Rect m_gameButton = Rect(Arg::center = Scene::Center().movedBy(0, 200), 300, 60);
-        Transition m_gameTransition = Transition(0.4s, 0.2s);
-
-        Rect m_plotButton = Rect(Arg::center = Scene::Center().movedBy(0, 300), 300, 60);
-        Transition m_plotTransition = Transition(0.4s, 0.2s);
-
-    public:
-
-        Title(const InitData& init);
-        void update() override;
-        void draw() const override;
+    Title(const InitData& init);
+    void update() override;
+    void draw() const override;
 
 };
